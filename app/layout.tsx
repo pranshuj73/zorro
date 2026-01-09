103ebd11dff0ed06a44034533b25934caf5a5bd9
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2026-01-10 02:56:45 +0530

    bg update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ConsoleSignature } from "@/components/console-signature";
+import { BackgroundGrid } from "@/components/background-grid";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground selection:bg-white selection:text-black`}
       >
         {children}
+        <BackgroundGrid />
         <ConsoleSignature />
       </body>
     </html>
